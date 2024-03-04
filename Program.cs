63ca01ce23195087442f894dcec770882442099f
CovenantDev <john.t.rathmell@gmail.com>
2024-03-04 15:58:37 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Test_000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This test initially created for GitHub archive testing...");
+        }
+    }
+}
